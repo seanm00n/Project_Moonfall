a190b6e9ea9e50a0e74206bc37803a7610e55078
seanm00n <deccj97@naver.com>
2023-03-07 16:45:25 +0900

    Initial commit

---
+++ Source/Project_Moonfall/Public/CustomWorldSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "CustomWorldSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_MOONFALL_API UCustomWorldSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+	
+};
