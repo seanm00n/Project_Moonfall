7e64b3fe054de9d73b797dd2efb943544e40f972
seanm00n <deccj97@naver.com>
2023-04-25 16:27:09 +0900

    feat Darknessy phase1,2
    fix walk cooltime

---
+++ Source/Project_Moonfall/Public/System/Enemy/BTTask_MoveToTimer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_MoveTo.h"
+#include "BTTask_MoveToTimer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_MOONFALL_API UBTTask_MoveToTimer : public UBTTask_MoveTo
+{
+	GENERATED_BODY()
+	
+};
